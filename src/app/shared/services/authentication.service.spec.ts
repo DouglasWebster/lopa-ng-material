9f60351a8a684ecf3efba4abb3568fe47a51b7e8
DouglasWebster <douglas.webster2107@gmail.com>
2017-08-08 16:35:19 +0100

    Aunthentication unit test updated to full coverage

--- src/app/shared/services/authentication.service.spec.ts
+++ src/app/shared/services/authentication.service.spec.ts
@@ -1,5 +1,11 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { HttpModule } from '@angular/http';
+import { TestBed, inject, async, fakeAsync, tick } from '@angular/core/testing';
+
+import { fakeBackendProvider } from '../../../tests/fake-backend';
+import { MockBackend } from '@angular/http/testing';
+import { BaseRequestOptions, HttpModule } from '@angular/http';
+
+import { UserService } from './user.service';
+import { User } from '../models/user';
 
 import { AuthenticationService } from './authentication.service';
 
@@ -9,13 +15,109 @@ describe('AuthenticationService', () => {
       imports: [
         HttpModule
       ],
+      declarations: [
+      ],
       providers: [
-        AuthenticationService
+        AuthenticationService,
+        UserService,
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
       ]
     });
   });
 
-  it('should ...', inject([AuthenticationService], (service: AuthenticationService) => {
+  beforeEach(() => {
+    localStorage.removeItem('users');
+    localStorage.removeItem('currentUser');
+  });
+
+  it('should d create an authentication service by injection', async(inject([AuthenticationService], (service: AuthenticationService) => {
     expect(service).toBeTruthy();
-  }));
+  })));
+
+  it('should report an error when trying to log in with no users created',
+    fakeAsync(inject([AuthenticationService, UserService], (check: AuthenticationService, service: UserService) => {
+
+      const user: User = { 'userName': 'dDuck', 'password': 'Quack', 'firstName': 'Donald', 'lastName': 'Duck', };
+      check.login(user.userName, user.password).subscribe(
+        data => {
+          fail('Login should be returning an error');
+        },
+        error => {
+          expect(error.message).toContain('Username or password is incorrect');
+          expect(localStorage.currentUser).toBeUndefined();
+        }
+      );
+      tick(500);
+    })));
+
+  it('should report an error when trying to log in with an invalid user',
+    fakeAsync(inject([AuthenticationService, UserService], (check: AuthenticationService, service: UserService) => {
+      const user: User = { 'userName': 'dDuck', 'password': 'Quack', 'firstName': 'Donald', 'lastName': 'Duck', };
+
+      service.create(user).subscribe(
+        data => { },
+        error => fail(error)
+      );
+      tick(500);
+
+      check.login('mMouse', 'Squeek').subscribe(
+        data => {
+          fail('Login should be returning an error');
+        },
+        error => {
+          expect(error.message).toContain('Username or password is incorrect');
+          expect(localStorage.currentUser).toBeUndefined();
+        }
+      );
+      tick(500);
+    })));
+
+  it('should set "localStorage.currentUser" to the user.userName when login successful',
+    fakeAsync(inject([AuthenticationService, UserService], (check: AuthenticationService, service: UserService) => {
+      const user: User = { 'userName': 'dDuck', 'password': 'Quack', 'firstName': 'Donald', 'lastName': 'Duck', };
+
+      service.create(user).subscribe(
+        data => { },
+        error => fail(error)
+      );
+      tick(500);
+
+      check.login('dDuck', 'Quack').subscribe(
+        data => {
+          expect(data).toBeUndefined();
+        },
+        error => fail(error)
+      );
+      tick(500);
+
+      expect(localStorage.currentUser).toContain('dDuck');
+    })));
+
+  it('should clear "localStorage.currentUser" when user logs out',
+    fakeAsync(inject([AuthenticationService, UserService], (check: AuthenticationService, service: UserService) => {
+      const user: User = { 'userName': 'dDuck', 'password': 'Quack', 'firstName': 'Donald', 'lastName': 'Duck', };
+
+      expect(localStorage.currentUser).toBeUndefined();
+
+      service.create(user).subscribe(
+        data => { },
+        error => fail(error)
+      );
+      tick(500);
+
+      check.login('dDuck', 'Quack').subscribe(
+        data => {
+          expect(data).toBeUndefined();
+        },
+        error => fail(error)
+      );
+      tick(500);
+
+      expect(localStorage.currentUser).toContain('dDuck');
+
+      check.logout();
+      expect(localStorage.currentUser).toBeUndefined();
+    })));
 });
