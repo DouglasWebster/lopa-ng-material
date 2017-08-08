d5cb8a38c8f2c31619f8dfbe9c47e5beff7c2418
Douglas Webster <douglas.webster2107@gmail.com>
2017-08-08 14:48:42 +0100

    Moved from shared to tests folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // used to create fake backend
-import { fakeBackendProvider } from './shared/helpers/fake-backend';
+import {fakeBackendProvider} from '../tests/fake-backend';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { BaseRequestOptions } from '@angular/http';
 
