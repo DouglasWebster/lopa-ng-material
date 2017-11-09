b63da1daff3234fdabd76fd873e2821610dc271d
Douglas Webster <douglas.webster2107@gmail.com>
2017-11-09 18:53:20 +0000

    Removed deprecated MaterialModule and import AppMaterialModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+import { AppMaterialModule } from './app.material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
 
 // used to create fake backend
-import {fakeBackendProvider} from '../tests/fake-backend';
+import { fakeBackendProvider } from '../tests/fake-backend';
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { BaseRequestOptions } from '@angular/http';
 
@@ -28,9 +28,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule
   ],
   providers: [
     AuthenticationService,
@@ -38,11 +38,11 @@ import { AppComponent } from './app.component';
     LoginGuard,
     AdminGuard,
 
-      // providers used to create fake backend
+    // providers used to create fake backend
     fakeBackendProvider,
     MockBackend,
     BaseRequestOptions
-],
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
