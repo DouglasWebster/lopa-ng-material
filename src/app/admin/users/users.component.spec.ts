82141f40b2270492be239bfc2ed1545dd7489f74
Douglas Webster <douglas.webster2107@gmail.com>
2017-12-03 12:45:58 +0000

    Initial build of User component

--- src/app/admin/users/users.component.spec.ts
+++ src/app/admin/users/users.component.spec.ts
@@ -1,25 +1,190 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { async, ComponentFixture, fakeAsync, flushMicrotasks, TestBed } from '@angular/core/testing';
+import { Component, ViewChild, Injectable, OnInit } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { UsersComponent } from './users.component';
+import { IUser } from '../../shared/models';
+import { UserService } from '../../shared/services/index';
 
-describe('UsersComponent', () => {
+@Injectable()
+export class FakeUserService {
+
+  users: IUser[] = [];
+
+  constructor() {
+    for (let i = 0; i < 4; i++) {
+      const nextIndex = this.users.length + 1;
+
+      const copiedData = this.users.slice();
+      copiedData.push({
+        firstName: `FN_${nextIndex}`,
+        lastName: `LN_${nextIndex}`,
+        userName: `fn_${nextIndex}`,
+        password: `pw_${nextIndex}`,
+        admin: ((nextIndex % 3) === 0),
+        id: nextIndex
+      });
+
+      this.users = copiedData;
+
+    }
+  }
+
+  getAll() {
+    return Observable.of(this.users);
+  }
+
+  getById(id: number) {
+    console.log(`UserService stub called with id of ${id}`);
+
+    const filteredUsers: IUser[] = this.users.filter(chkUser => {
+      return chkUser.id === id;
+    });
+
+    if (filteredUsers.length === 1) {
+      return Observable.of(filteredUsers);
+    } else {
+      if (filteredUsers.length === 0) {
+        return Observable.throw(Error(`No User with ${id} found`));
+      } else {
+        return Observable.throw(Error(`Multiple user with same ID ${id} found`));
+      }
+    }
+  }
+
+  create(newUser: IUser) {
+
+    // validation
+    const duplicateUser = this.users.filter(user => user.userName === newUser.userName).length;
+    if (duplicateUser) {
+      return Observable.throw(Error('Username "' + newUser.userName + '" is already taken'));
+    }
+
+    // do we have an admin account? If not make give this user admin rights.
+    const adminUser = this.users.find(user => user.admin === true);
+    if (!adminUser) {
+      newUser.admin = true;
+    } else {
+      newUser.admin = false;
+    }
+
+    // save new user
+    // let maxId = 0;
+    // newUser.id = this.users.map<IUser>(obj => {
+    //   if (obj.id >= maxId) { maxId = obj.id + 1; }
+    // })[0].id;
+    this.users.push(newUser);
+
+    return;
+  }
+
+  update(user: IUser) {
+
+    let userFound = false;
+    // find user by id in users array
+    const id = user.id;
+    for (let i = 0; i < this.users.length; i++) {
+      const testUser = this.users[i];
+      if (testUser.id === id) {
+        userFound = true;
+        this.users[i].id = user.id;
+        this.users[i].admin = user.admin;
+        this.users[i].userName = user.userName;
+        this.users[i].firstName = user.firstName;
+        this.users[i].lastName = user.lastName;
+        break;
+      }
+    }
+    if (userFound) {
+      return;
+    } else {
+      return Observable.throw(Error(`User ${user.userName} not found`));
+    }
+  }
+
+  delete(id: number) {
+
+    // find user by id in users array
+    for (let i = 0; i < this.users.length; i++) {
+      if (this.users[i].id === id) {
+        // delete user
+        this.users.splice(i, 1);
+        break;
+      }
+    }
+
+    return;
+  }
+}
+
+
+fdescribe('UsersComponent', () => {
+
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      imports: [
+        MatToolbarModule,
+        MatTableModule,
+        MatSortModule,
+        NoopAnimationsModule
+      ],
+      declarations: [
+        UsersComponent
+      ],
+      providers: [
+        { provide: UserService, useClass: FakeUserService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it('should be created with 4 rows of data', () => {
+
+    // tslint:disable-next-line:no-non-null-assertion
+    const tableElement = fixture.nativeElement.querySelector('.mat-table')!;
+    const tblRows = getRows(tableElement).length;
     expect(component).toBeTruthy();
+    expect(tblRows).toEqual(4);
   });
+
+  it('should have 3 rows after 1 item is deleted', () => {
+    // component.user
+
+
+  });
+
 });
+
+// Utilities copied from CDKTable's spec
+function getElements(element: Element, query: string): Element[] {
+  return [].slice.call(element.querySelectorAll(query));
+}
+
+function getHeaderRow(tableElement: Element): Element {
+  // tslint:disable-next-line:no-non-null-assertion
+  return tableElement.querySelector('.mat-header-row')!;
+}
+
+function getRows(tableElement: Element): Element[] {
+  return getElements(tableElement, '.mat-row');
+}
+function getCells(row: Element): Element[] {
+  return row ? getElements(row, '.mat-cell') : [];
+}
--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,5 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { MatSort } from '@angular/material/sort';
+
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+
+import { UserService } from '../../shared/services';
+import { IUser } from '../../shared/models/';
+
+export class UsersDataSource extends DataSource<any> {
+
+  constructor(private _subject: BehaviorSubject<IUser[]>, private _sort: MatSort) {
+    super();
+  }
+
+  connect(): Observable<IUser[]> {
+    const displayDataChanges = [
+      this._subject.value,
+      this._sort.sortChange
+    ];
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      return this.getSortedData();
+    });
+
+  }
+
+  disconnect(): void { }
+
+  getSortedData(): IUser[] {
+    const data = this._subject.value;
+    if (!this._sort.active || this._sort.direction === '') { return data; }
+
+    return data.sort((a, b) => {
+      let propertyA: number | string = '';
+      let propertyB: number | string = '';
+
+      switch (this._sort.active) {
+        case 'firstName': [propertyA, propertyB] = [a.firstName, b.firstName]; break;
+        case 'lastName': [propertyA, propertyB] = [a.lastName, b.lastName]; break;
+      }
+
+      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+
+      return (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1);
+    });
+  }
+}
 @Component({
   selector: 'lpa-users',
   templateUrl: './users.component.html',
@@ -7,9 +60,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['firstName', 'lastName', 'admin'];
+  dataSource: UsersDataSource | null;
+  dataSubject: BehaviorSubject<IUser[]> = new BehaviorSubject<IUser[]>([]);
+  users: IUser[] = [];
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private userService: UserService
+  ) {
+  }
 
   ngOnInit() {
+    console.log('In the ngOnInitFunction');
+    this.userService.getAll().subscribe(
+      data => {
+        console.log('Initial data: ', data);
+        this.users = <IUser[]>data;
+        this.dataSubject.next(this.users);
+        this.dataSource = new UsersDataSource(this.dataSubject, this.sort);
+        this.sort.active = 'firstName';
+        this.sort.direction = 'asc';
+
+        console.log('Registered Users: ', this.users);
+      },
+      error => {
+        console.log('Failed to get users: ', error);
+      }
+    );
+  }
+
+  handleRowClick(row) {
+    console.log('handleRowClick passed: ', row);
+
+  }
+
+  handleCellClick(cell) {
+    console.log('handleCellClick passed: ', cell);
+
+
+  }
+
+  removeUser(exUser: IUser) {
+    const id = exUser.id;
+
+    this.userService.delete(id).subscribe(
+      result => { },
+      error => { }
+    );
+
   }
 
 }
